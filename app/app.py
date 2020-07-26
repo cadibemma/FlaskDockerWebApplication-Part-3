f4a7bd66be1b7e42b1f57ec596ac9fe1f3d600ad
cadibemma <chikaadibemma@live.com>
2020-07-26 16:45:26 -0400

    Task: added content to view.html and updated app.py with view page

--- app/app.py
+++ app/app.py
@@ -15,13 +15,23 @@ app.config['MYSQL_DATABASE_PORT'] = 3306
 app.config['MYSQL_DATABASE_DB'] = 'biostatsData'
 mysql.init_app(app)
 
+
 @app.route('/', methods=['GET'])
 def index():
-    user = {'username': 'Biostats Project'}
+    user = {'username': 'Chika"s Project'}
     cursor = mysql.get_db().cursor()
     cursor.execute('SELECT * FROM biostatsData')
     result = cursor.fetchall()
     return render_template('index.html', title='Home', user=user, biostats=result)
+
+
+@app.route('/view/<int:stat_id>', methods=['GET'])
+def record_view(stat_id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM biostatsData WHERE id=%s', stat_id)
+    result = cursor.fetchall()
+    return render_template('view.html', title='View Form', city=result[0])
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
